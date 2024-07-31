b901da23b94b00caef24c17726a71bba525a3556
Ritika <ritz21ch@gmail.com>
2024-07-31 19:30:11 +0530

    update admin,forms and dog_list and media file is created

--- dogs/admin.py
+++ dogs/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Dog,Breed
 # Register your models here.
+admin.register(Dog)
+admin.register(Breed)
--- dogs/forms.py
+++ dogs/forms.py
@@ -6,3 +6,6 @@ class DogForm(forms.ModelForm):
     class Meta:
         model = Dog
         fields = ['name', 'breed', 'dob', 'gender', 'size', 'energy_level', 'temperament', 'address', 'vaccination_status', 'photo', 'bio']
+        widgets = {
+            'dob': forms.DateInput(attrs={'type': 'date'}),  # This will use a date picker in modern browsers
+        }
